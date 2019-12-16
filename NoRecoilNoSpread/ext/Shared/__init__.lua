6e925a07db9dada4215a32e29b76e45e0c176dca
FoolHen <fulgen_3_7ad@hotmail.com>
2019-12-16 13:34:42 +0100

    Updated NoRecoilNoSpread to only affect M4A1.

--- NoRecoilNoSpread/ext/Shared/__init__.lua
+++ NoRecoilNoSpread/ext/Shared/__init__.lua
@@ -4,12 +4,12 @@ Events:Subscribe('Partition:Loaded', function(partition)
 
 	for _, instance in pairs(instances) do
 		-- Check if the instance is of the type we are looking for.
-		if instance.typeInfo.name == 'GunSwayData' then
+		if instance.instanceGuid == Guid('73D73EFD-F082-4C13-8253-24E4FEB39244') then
 			-- Cast it so we can access its fields.
 			local gunSwayData = GunSwayData(instance)
 			-- Make it writable so we can modify its fields.
 			gunSwayData:MakeWritable()
-			
+
 			gunSwayData.deviationScaleFactorZoom = 0
 			gunSwayData.gameplayDeviationScaleFactorZoom = 0
 			gunSwayData.deviationScaleFactorNoZoom = 0
