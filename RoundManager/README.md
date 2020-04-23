7ae397becd40e3b286f80339c560209c1d0b3bb3
FoolHen <fulgen_3_7ad@hotmail.com>
2020-04-23 18:34:46 +0200

    Added RoundManager.

---
+++ RoundManager/README.md
@@ -0,0 +1,36 @@
+# Round Manager
+
+A simple file that you can add to your mods to help with managing round logic.
+
+## API
+
+If you want to use this in your mods simply copy the `round.lua` script to your mod's server folder, and require the class in a server script with ``local Round = require('Round')``. Then create an instance, for example ``local m_Round = Round(number roundTime[, number minPlayers, number preRoundTime, number roundOverTime, boolean announceInChat])``. The constructor parameters are the following:
+
+| Argument | Description |
+| ------ | ----------- |
+| `number roundTime` | Sets the round time. |
+| `number minPlayers` | Optional. Sets the minimum players that have loaded in required to start the pre-round/round. Default is ``2``. |
+| `number preRoundTime` | Optional. Sets the time of the pre-round state. Can be set to ``0`` (no pre-round) or ``-1`` (infinite, in case you want to start the round from your mod). Default is ``0``. |
+| `number roundOverTime` | Optional. Sets the time of the round over state. Can be set to ``0`` (no pre-round) or ``-1`` (infinite). Default is ``-1``. |
+| `boolean announceInChat` | Optional. If set to ``true`` round info will be sent in chat. Defaults to ``true``. |
+
+| Method | Description |
+| ------ | ----------- |
+| `(Constructor) Round(number roundTime[, number minPlayers, number preRoundTime, number roundOverTime, boolean announceInChat])` | Instantiates the round. ``preRoundTime`` and ``roundOverTime`` can be set to ``0`` or ``-1`` to disable the round state or make its time infinite respectively. ``preRoundTime`` is ``0`` by default and ``roundOverTime`` is infinite.|
+| `Guid[] getPlayersReady()` | Returns an array with the Guids of players that have loaded in. |
+| `RoundState getRoundState()` | Returns the current state as an enum. You can find the enum in ``round.lua``. |
+| `void startPreRound()` | Starts the pre-round. |
+| `void startRound()` | Starts the round. |
+| `void endRound()` | Ends the round. |
+
+The script also exposes some custom events:
+
+| Event | Description |
+| ------ | ----------- |
+| `"Round:Time" (number time)` | Called when the current round time updates. Has a param with the time. |
+| `"Round:WaitingForPlayers"` | Called when the round state changes to waiting for players. |
+| `"Round:PreRoundStart"` | Called when the round state changes to pre-round. |
+| `"Round:RoundStart"` | Called when the round state changes to in-game. |
+| `"Round:RoundOver"` | Called when the round state changes to round over. |
+
+For an example on how to use this API refer to [BoxGame](https://github.com/Raikem/BoxGame) mod.
---
+++ RoundManager/round.lua
@@ -0,0 +1,248 @@
+class 'Round'
+
+RoundState = {
+	WaitingForPlayers = 0,
+	PreRound = 1,
+	Running = 2,
+	RoundOver = 3
+}
+
+local UPDATE_RATE = 0.5
+local DEFAULT_MIN_PLAYERS = 2
+
+function Round:__init(roundTime, minPlayers, preRoundTime, roundOverTime, announceInChat)
+	Events:Subscribe('Engine:Update', self, self._onUpdate)
+	Events:Subscribe('Player:Destroyed', self, self._onPlayerDestroyed)
+
+	NetEvents:Subscribe('BoxGame:PlayerReady', self, self._onPlayerReady)
+
+	self._minPlayers = minPlayers or DEFAULT_MIN_PLAYERS
+	self._announceInChat = announceInChat or true
+	self._roundState = RoundState.WaitingForPlayers
+	self._accumulatedDelta = 0
+
+	self._maxPreRoundTime = preRoundTime or 0 -- No preround by default.
+	self._maxRoundOverTime = roundOverTime or -1 -- Infinite roundover time by default.
+	self._maxRoundTime = roundTime
+
+	self._playersReady = {}
+
+	self:_resetVars()
+end
+
+function Round:_resetVars()
+	self._currentRoundTime = 0
+	self._currentRoundOverTime = 0
+	self._currentPreRoundTime = 0
+end
+
+function Round:getPlayersReady()
+	return self._playersReady
+end
+
+function Round:getRoundState()
+	return self._roundState
+end
+
+function Round:startPreRound()
+	self:_setRoundState(RoundState.PreRound)
+end
+
+function Round:startRound()
+	self:_setRoundState(RoundState.Running)
+end
+
+function Round:endRound()
+	self:_setRoundState(RoundState.RoundOver)
+end
+
+function Round:_onPlayerReady(player)
+	print('player '..player.name..' ready')
+	table.insert(self._playersReady, player.guid)
+end
+
+function Round:_onPlayerDestroyed(player)
+	for i, playerGuid in pairs(self._playersReady) do
+		if playerGuid == player.guid then
+			table.remove(self._playersReady, i)
+			return
+		end
+	end
+end
+
+function Round:_numOfPlayersReady()
+	local n = 0
+	for _, __ in pairs(self._playersReady) do
+		n = n + 1
+	end
+	return n
+end
+
+
+function Round:_onUpdate(delta, simulationDelta)
+	self._accumulatedDelta = self._accumulatedDelta + delta
+
+	if self._accumulatedDelta < UPDATE_RATE then
+		return
+	end
+
+	-- If the game state is in waiting for players
+	if self._roundState == RoundState.WaitingForPlayers then
+		self:_onWaitingForPlayers()
+	end
+
+	-- If the game state is in preround
+	if self._roundState == RoundState.PreRound then
+		self:_onPreRound(self._accumulatedDelta)
+	end
+	
+	-- If the game state is running
+	if self._roundState == RoundState.Running then
+		self:_onRunning(self._accumulatedDelta)
+	end
+	
+	-- If the game state is in game over
+	if self._roundState == RoundState.RoundOver then
+		self:_onRoundOver(self._accumulatedDelta)
+	end
+
+	self._accumulatedDelta = 0
+end
+
+function Round:_onWaitingForPlayers()
+	if self._roundState ~= RoundState.WaitingForPlayers then
+		return
+	end
+
+	local playerCount = self:_numOfPlayersReady()
+
+	if playerCount >= self._minPlayers then
+		-- Skip preround if the max time is 0.
+		if self._maxPreRoundTime == 0 then
+			self:_setRoundState(RoundState.Running)
+		else
+			self:_setRoundState(RoundState.PreRound)
+		end
+	end
+end
+
+function Round:_onPreRound(delta)
+	if self._roundState ~= RoundState.PreRound then
+		return
+	end
+
+	local playerCount = self:_numOfPlayersReady()
+
+	-- Min players condition not reached.
+	if playerCount < self._minPlayers then
+		self:_setRoundState(RoundState.WaitingForPlayers)
+		return
+	end
+
+	-- Ignore if preround is infinite.
+	if self._maxPreRoundTime < 0 then
+		return
+	end
+
+	-- Time Expires.
+	if self._currentPreRoundTime >= self._maxPreRoundTime then
+		self:_setRoundState(RoundState.Running)
+		return
+	end
+
+	self._currentPreRoundTime = math.min(self._currentPreRoundTime + delta, self._maxPreRoundTime)
+
+	-- Check every second to announce how many seconds are left.
+	if self._currentPreRoundTime - math.floor(self._currentPreRoundTime) < UPDATE_RATE then
+		ChatManager:SendMessage('Round starts in ' .. math.ceil((self._maxPreRoundTime - self._currentPreRoundTime)))
+	end
+
+	-- print('Preround time: '..self._currentPreRoundTime)
+end
+
+function Round:_onRunning(delta)
+	if self._roundState ~= RoundState.Running then
+		return
+	end
+
+	local playerCount = self:_numOfPlayersReady()
+
+	-- Min players condition not reached.
+	if playerCount < self._minPlayers then
+		self:_setRoundState(RoundState.WaitingForPlayers)
+		return
+	end
+
+	-- Ignore if round is infinite.
+	if self._maxPreRoundTime < 0 then
+		return
+	end
+	
+	-- Time Expires.
+	if self._currentRoundTime >= self._maxRoundTime then
+		self:_setRoundState(RoundState.RoundOver)
+		return
+	end
+
+	-- Check every minute to announce how many minutes are left.
+	if self._currentRoundTime % 60 <= UPDATE_RATE and self._currentRoundTime ~= 0 and self._announceInChat then
+		ChatManager:SendMessage(math.ceil((self._maxRoundTime - self._currentRoundTime)/60) .. ' minutes left.')
+	end
+	
+	-- Tick the current round time forward.
+	self._currentRoundTime = math.min(self._currentRoundTime + delta, self._maxRoundTime)
+	-- print('Round time: '..self._currentRoundTime)
+	Events:Dispatch('Round:Time', math.floor(self._currentRoundTime))
+end
+
+function Round:_onRoundOver(delta)
+	if self._roundState ~= RoundState.RoundOver then
+		return
+	end
+
+	-- Ignore if roundover is infinite.
+	if self._maxRoundOverTime < 0 then
+		return
+	end
+
+	-- Time Expires, restart round.
+	if self._currentRoundOverTime >= self._maxRoundOverTime then
+		-- Skip preround if the max time is 0.
+		if self._maxPreRoundTime == 0 then
+			self:_setRoundState(RoundState.Running)
+		else
+			self:_setRoundState(RoundState.PreRound)
+		end
+		return
+	end
+
+	self._currentRoundOverTime = math.min(self._currentRoundOverTime + delta, self._maxRoundOverTime)
+	-- print('Round over time: '..self._currentRoundOverTime)
+end
+
+function Round:_setRoundState(state)
+	self:_resetVars()
+	self._roundState = state
+
+	local announceMessage
+
+	if self._roundState == RoundState.WaitingForPlayers then
+		Events:Dispatch('Round:WaitingForPlayers')
+		announceMessage = 'Round stopped. Waiting for more players.'
+	elseif self._roundState == RoundState.PreRound then
+		Events:Dispatch('Round:PreRoundStart')
+		announceMessage = 'Preround started.'
+	elseif self._roundState == RoundState.Running then
+		Events:Dispatch('Round:RoundStart')
+		announceMessage = 'Round started.'
+	elseif self._roundState == RoundState.RoundOver then
+		Events:Dispatch('Round:RoundOver')
+		announceMessage = 'Round over.'
+	end
+
+	if self._announceInChat then
+		ChatManager:SendMessage(announceMessage)
+	end
+end
+
+return Round
